e3dc92311be17dc226c09810e21a82337ca42e1f
simreteabmekbib <se.simreteab.mekbib@gmail.com>
2021-08-06 00:11:03 -0700

    add grid view to home page, drawer

---
+++ lib/drawer/drawer.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Defaults {
+  static final Color drawerItemColor = Colors.blueGrey;
+  static final Color drawerItemSelectedColor = Colors.white;
+  static final Color drawerSelectedTileColor = Color(0xFFFF00FF);
+
+  static final drawerItemText = [
+    'Channels',
+    'Filter',
+    'Favorite',
+    'Share',
+    'Watchlist',
+    'about us',
+  ];
+
+  static final drawerItemIcon = [
+    Icons.tv,
+    Icons.search,
+    Icons.favorite,
+    Icons.share,
+    Icons.watch_later,
+    Icons.info,
+  ];
+}
