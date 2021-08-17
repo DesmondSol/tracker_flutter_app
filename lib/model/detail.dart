16eb5ca709de36bdfa6d3a99377ec1584b8e83fe
DesmondSol <soltig66@gmail.com>
2021-08-17 19:05:49 +0300

    changed depriciated button

--- lib/model/detail.dart
+++ lib/model/detail.dart
@@ -12,24 +12,32 @@ class Detail extends StatefulWidget {
 class _DetailState extends State<Detail> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text(''),
-        backgroundColor: Color(0xFFFF00FF),
-),
-      body: Container(
-        alignment: Alignment.center,
-        margin: EdgeInsets.all(30),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Hero(tag: 'image${widget.photo.id}', 
-            child: FadeInImage.assetNetwork(placeholder: 'assets/images/no-image.png', image: widget.photo.url),),
-            SizedBox(height: 30,),
-            OutlineButton(
-              child: Icon(Icons.close),
-              onPressed: () => Navigator.of(context).pop())
-          ],
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('channel name'),
+          backgroundColor: Color(0xFFFF00FF),
         ),
-      ));
+        body: Container(
+          alignment: Alignment.center,
+          margin: EdgeInsets.all(30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Hero(
+                tag: 'image${widget.photo.id}',
+                child: FadeInImage.assetNetwork(
+                    placeholder: 'assets/images/no-image.png',
+                    image: widget.photo.url),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              OutlinedButton(
+                  child: Icon(Icons.close),
+                  onPressed: () => Navigator.of(context).pop())
+            ],
+          ),
+        ));
   }
 }
